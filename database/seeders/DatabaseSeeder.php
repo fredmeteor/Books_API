30f524ff6e5ac52031ea0dbd7a7d43d65d6fd730
fredmeteor <fredpeter09899@gmail.com>
2023-05-15 17:46:36 +0300

    Added BooksTableSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -16,6 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(BookingTableSeeder::class); 
+        Model::unguard();
+// $this->call('UserTableSeeder');
+$this->call(BooksTableSeeder::class);
+      Model::reguard();
     }
 }
